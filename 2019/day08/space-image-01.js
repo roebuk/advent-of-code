630670aa672b6714b7edcda50a48d1c811b0f09c
Kristian Roebuck <roebuck86@gmail.com>
2019-12-08 12:19:11 +0000

    Day 8 part 1

---
+++ 2019/day08/space-image-01.js
@@ -0,0 +1,30 @@
+const { readFile } = require("fs")
+const R = require('ramda')
+
+const getNumberOfZeros = R.pipe(
+  R.match(/[0]/ug),
+  R.length
+)
+
+readFile("./input.txt", "utf-8", (_err, data) => {
+  const input = R.pipe(
+    R.trim,
+    R.splitEvery(6 * 25),
+    R.reduce(((acc, item) => {
+      if (acc === null) {
+        return item
+      }
+      const itemsLength = getNumberOfZeros(item)
+      const prevLength = getNumberOfZeros(acc)
+
+      return itemsLength < prevLength ? item : acc
+    }), null),
+    item => {
+      const ones = R.match(/[1]/ug, item).length
+      const twos = R.match(/[2]/ug, item).length
+
+      return ones * twos
+    },
+    R.tap(console.log)
+  )(data)
+});
